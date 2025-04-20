1875f96d63ed624aa94fc2ecdfa873b0ab3e93db
tiyam <tiyambakhshian@gmail.com>
2025-04-21 00:36:29 +0330

    Add Admin Area Controller Views

---
+++ Refhub_Ir/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Refhub_Ir.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
