515bccf80fc94706ceea42536f6d6e7a31060269
amirhossein33 <shiravandispdf@gmail.com>
2025-04-23 02:32:09 +0330

    Commit

--- Refhub_Ir/Controllers/HomeController.cs
+++ Refhub_Ir/Controllers/HomeController.cs
@@ -22,7 +22,14 @@ namespace Refhub_Ir.Controllers
         {
             return View();
         }
-
+        public IActionResult Contact()
+        {
+            return View();
+        } 
+        public IActionResult CommingSoon()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
