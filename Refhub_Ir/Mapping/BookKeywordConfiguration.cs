62a08161d3fc69220aa570b11ec1017b7df52c09
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-04-27 15:50:49 -0800

    Create and Design BookKeyword

---
+++ Refhub_Ir/Mapping/BookKeywordConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Refhub_Ir.Models.Entities;
+
+namespace Refhub_Ir.Mapping
+{
+    public class BookKeywordConfiguration : IEntityTypeConfiguration<BookKeyword>
+    {
+        public void Configure(EntityTypeBuilder<BookKeyword> builder)
+        {
+            builder.HasOne(bk => bk.Book).WithMany(b => b.BookKeywords)
+                .HasForeignKey(bk => bk.BookId);
+
+            builder.HasOne(bk => bk.Keyword).WithMany(b => b.BookKeywords)
+                .HasForeignKey(bk => bk.KeywordId);
+        }
+    }
+}
