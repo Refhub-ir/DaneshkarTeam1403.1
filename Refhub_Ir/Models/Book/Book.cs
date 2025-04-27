6dd3ada7d2b1f863f7198f1b0a34db771364ab78
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-04-27 13:08:24 -0800

    add book entity

---
+++ Refhub_Ir/Models/Book/Book.cs
@@ -0,0 +1,25 @@
+﻿namespace Refhub_Ir.Models.Book
+{
+    public class Book
+    {
+        // Primary Key
+        public int Id { get; set; }
+
+        // Basic Properties
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public int PageCount { get; set; }
+        public string FilePath { get; set; }
+
+        // Foreign Key
+        public int CategoryId { get; set; }
+
+        // Navigation Properties
+        // Preventing NullReferenceException by initializing collections.
+        public Category Category { get; set; }
+        public List<BookKeyword> BookKeywords { get; set; } = new();
+        public List<BookAuthor> BookAuthors { get; set; } = new();
+        public List<BookRelation> RelatedTo { get; set; } = new();
+        public List<BookRelation> RelatedFrom { get; set; } = new();
+    }
+}
