02827c143c5ae46661b30c1b3331f04456a052ff
mohammad mehdi khodabande <mmahdikhodabande@gmail.com>
2025-04-28 13:00:07 +0330

    Add DTO's about login and register

---
+++ Refhub_Ir/Models/DTO/User/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Refhub_Ir.Models.DTO.User
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "ایمیل خود را وارد کنید")]
+        [EmailAddress(ErrorMessage = "فرمت ایمیل درست نیست")]
+        public string Email { get; set; }
+
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$", ErrorMessage = "حداقل یک حرف انگلیسی داشته باشه|حداقل یک عدد هم داشته باشه|طولش حداقل ۶ کاراکتر باشه")]
+        [Required(ErrorMessage = "رمز عبورفراموش نشه ")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$", ErrorMessage = "حداقل یک حرف انگلیسی داشته باشه|حداقل یک عدد هم داشته باشه|طولش حداقل ۶ کاراکتر باشه")]
+        [Required(ErrorMessage = "تکرار رمز خود را وارد کن")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "رمز عبور و تکرار آن یکسان نیست .")]
+        public string ConfirmPassword { get; set; }
+    }
+}
