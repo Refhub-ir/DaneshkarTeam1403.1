6e7d5faaaf539ab33366925f46800fde00ac7329
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-04-27 17:14:53 -0800

    Move Mpaaing Foulder to Data Foulder

---
+++ Refhub_Ir/Data/Mapping/AuthorConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Refhub_Ir.Models.Entities;
+
+namespace Refhub_Ir.Mapping
+{
+    public class AuthorConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.HasKey(a=>a.Id);
+            builder.Property(a=>a.FullName).IsRequired().HasMaxLength(256);
+            builder.Property(a => a.Slug).IsRequired();
+
+            builder.HasMany(a => a.BookAuthors)
+            .WithOne(ba => ba.Author)
+            .HasForeignKey(ba => ba.AuthorId);
+        }
+    }
+}
---
+++ Refhub_Ir/Data/Mapping/BookKeywordConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Refhub_Ir.Models.Entities;
+
+namespace Refhub_Ir.Mapping
+{
+    public class BookKeywordConfiguration : IEntityTypeConfiguration<BookKeyword>
+    {
+        public void Configure(EntityTypeBuilder<BookKeyword> builder)
+        {
+            builder.HasKey(bk => new { bk.BookId, bk.KeywordId });
+
+            builder.HasOne(bk => bk.Book).WithMany(b => b.BookKeywords)
+                .HasForeignKey(bk => bk.BookId);
+
+            builder.HasOne(bk => bk.Keyword).WithMany(b => b.BookKeywords)
+                .HasForeignKey(bk => bk.KeywordId);
+        }
+    }
+}
