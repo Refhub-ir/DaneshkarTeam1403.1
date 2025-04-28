f5c2e404d58d9cbc6e5971982fa0468746d302b4
mohammad mehdi khodabande <mmahdikhodabande@gmail.com>
2025-04-28 13:03:31 +0330

    Add master controller about login , register

---
+++ Refhub_Ir/Controllers/AccountController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Refhub_Ir.Data.Models.User;
+using Refhub_Ir.Models.DTO.User;
+
+namespace Refhub_Ir.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        #region Register
+        [HttpGet]
+        public IActionResult Register() => View();
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    string url = "/Account/Login";
+                    return Redirect(url);
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    if (error.Code == "DuplicateUserName" || error.Code == "DuplicateEmail")
+                    {
+                        ModelState.AddModelError("Email", "ایمیل وارد شده قبلاً ثبت شده است.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+
+            }
+            return View(model);
+        }
+        #endregion
+
+        #region Login
+        [HttpGet]
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                    return RedirectToAction("Index", "Home");
+
+                ModelState.AddModelError("", "Invalid login attempt.");
+            }
+            return View(model);
+        }
+        #endregion
+
+        #region Logout
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+        #endregion
+
+        public IActionResult AccessDenied() => View();
+    }
+}
+
+
