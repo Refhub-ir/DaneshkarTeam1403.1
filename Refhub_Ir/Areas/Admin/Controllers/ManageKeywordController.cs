736251a187d24f6bbfb570c2c245c7b7c3299809
Mohammad Faraji <m.faraji9841@gmail.com>
2025-04-30 00:59:23 +0330

    Create ManageKeywordController

---
+++ Refhub_Ir/Areas/Admin/Controllers/ManageKeywordController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Refhub_Ir.Areas.Admin.Controllers
+{
+    public class ManageKeywordController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateKeyword()
+        {
+
+            return View();
+        }
+    }
+}
