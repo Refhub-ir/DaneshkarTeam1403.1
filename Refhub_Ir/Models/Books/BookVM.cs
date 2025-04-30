c8adc35716d80746d754ad5b678fdb8bf9f6dc5a
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-30 10:20:18 +0330

    Add viewmodel bookVm

---
+++ Refhub_Ir/Models/Books/BookVM.cs
@@ -0,0 +1,19 @@
+﻿namespace Refhub_Ir.Models.Books
+{
+    public class BookVM
+    {
+        public int Id { get; set; }
+
+
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public int PageCount { get; set; }
+        public string FilePath { get; set; }
+        public string ImagePath { get; set; }
+
+        // Foreign Key
+        public int CategoryId { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
