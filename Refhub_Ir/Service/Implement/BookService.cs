9c39ae886f7b523f64f931a00d13f8ced314293c
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-30 10:20:51 +0330

    Create BookService

---
+++ Refhub_Ir/Service/Implement/BookService.cs
@@ -0,0 +1,33 @@
+﻿using Refhub_Ir.Models.Books;
+using Refhub_Ir.Service.Interface;
+
+namespace Refhub_Ir.Service.Implement
+{
+    public class BookService: IBookService
+    {
+        public Task<ICollection<BookVM>> GetBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<BookVM>> GetBook(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CreateBook(CreateBookVM book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateBook(UpdateBookVM book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
