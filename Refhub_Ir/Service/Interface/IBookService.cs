72e2bde8df33554c04363fffe6c878ee41041de7
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-30 10:21:03 +0330

    Create service iBookService

---
+++ Refhub_Ir/Service/Interface/IBookService.cs
@@ -0,0 +1,14 @@
+﻿using Refhub_Ir.Models.Books;
+
+namespace Refhub_Ir.Service.Interface
+{
+    public interface IBookService
+    {
+        Task<ICollection<BookVM>> GetBooks();
+        Task<ICollection<BookVM>> GetBook(int Id);
+        Task<bool>CreateBook(CreateBookVM book);
+        Task<bool>UpdateBook(UpdateBookVM book);
+        Task<bool>Delete(int Id);
+
+    }
+}
