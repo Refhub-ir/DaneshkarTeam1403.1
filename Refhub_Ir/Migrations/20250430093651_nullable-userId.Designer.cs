7998c9574c4453372accccb91562ee30c35b2991
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-30 18:21:14 +0330

    add migration nullableUserId

--- Refhub_Ir/Migrations/20250430093651_nullable-userId.Designer.cs
+++ Refhub_Ir/Migrations/20250430093651_nullable-userId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Refhub_Ir.Data.Context;
 
@@ -11,9 +12,11 @@ using Refhub_Ir.Data.Context;
 namespace Refhub_Ir.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250430093651_nullable-userId")]
+    partial class nullableuserId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
