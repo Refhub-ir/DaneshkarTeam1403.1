89e8e70c93022a7e2c69d10a6a432cd0dceabdb3
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-30 18:24:42 +0330

    Add FolderName Static

---
+++ Refhub_Ir/Tools/Static/FolderNameStatic.cs
@@ -0,0 +1,9 @@
+﻿namespace Refhub_Ir.Tools.Static
+{
+    public static class FolderNameStatic
+    {
+        public static string GetDirectoryImages = "Images";
+        public static string GetDirectoryBooks = "Books";
+        public static string GetDirectoryFiles = "Files";
+    }
+}
