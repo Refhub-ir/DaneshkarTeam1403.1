0aad8f01cab45b0bb7a97bc849f4e57b3e508bf7
Mohammad Faraji <m.faraji9841@gmail.com>
2025-04-30 03:43:27 +0330

    Create EditKeywordVM View Model

---
+++ Refhub_Ir/Models/DTO/Keyword/EditKeywordVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Refhub_Ir.Models.DTO.Keyword
+{
+    public class EditKeywordVM
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "کلمه کلیدی الزامی است")]
+        public string Word { get; set; }
+    }
+}
