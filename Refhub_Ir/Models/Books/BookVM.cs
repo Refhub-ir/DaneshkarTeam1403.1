bdd50ac25dae72cac64b3fc9d4e6fa2d6ffd3843
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-30 18:23:31 +0330

    Add BooVM

--- Refhub_Ir/Models/Books/BookVM.cs
+++ Refhub_Ir/Models/Books/BookVM.cs
@@ -7,13 +7,23 @@
 
         public string Title { get; set; }
         public string Slug { get; set; }
-        public int PageCount { get; set; }
-        public string FilePath { get; set; }
+        //public int PageCount { get; set; }
+        //public string FilePath { get; set; }
         public string ImagePath { get; set; }
 
         // Foreign Key
-        public int CategoryId { get; set; }
+        //public int CategoryId { get; set; }
 
         public string UserId { get; set; }
+    }
+
+    public class CategoryDropDownVM
+    {
+
+        public int Id { get; set; }
+
+
+        public string Name { get; set; }
+        public bool IsSelected { get; set; } = false;
     }
 }
