5ac70d2b6e65512d8fb0a58099d4b6af04838945
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-03 10:28:35 -0800

    nullable all strings proptery

--- Refhub_Ir/Data/Models/Category.cs
+++ Refhub_Ir/Data/Models/Category.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
-        public string slug { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? slug { get; set; }
+        public string? Description { get; set; }
 
         public List<Book> Books{ get; set; }
     }
