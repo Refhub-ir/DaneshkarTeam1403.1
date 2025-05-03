1f713833163f16da97db327cfac1d3c155326350
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-03 16:12:19 +0330

    Add Extention Use GenericPathComponent

---
+++ Refhub_Ir/Tools/ExtentionMethod/ViewComponentExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+public static class ViewComponentExtensions
+{
+    public static string GetDefaultViewPath(this ViewComponent viewComponent)
+    {
+        var type = viewComponent.GetType();
+
+        // ProFileUser_ProfileLayoutViewComponent => ProFileUser_ProfileLayout
+        var componentName = type.Name.Replace("ViewComponent", "View");
+
+        // Handler\User\Layout
+        var @namespace = type.Namespace.Replace("Handlers", "Views"); // OurResumeIR.MVC.ViewComponents.Handler.User.Layout
+        var folders = @namespace.Split('.')
+            .SkipWhile(p => p != "Components")
+            .Skip(1) // Skip "ViewComponents"
+            .ToArray();
+
+        // Handler -> User -> Layout
+        var path =string.Join('/',folders) ;
+
+        // ~/ViewComponents/Views/User/Layout/ProFileUser_ProfileLayoutView.cshtml
+        return $"~/Components/{path}/{componentName}.cshtml";
+    }
+}
