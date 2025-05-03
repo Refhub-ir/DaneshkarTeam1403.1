4f2bdc75f1b8654111db2a4bba2f0a68d7c388e1
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-03 15:53:07 +0330

    Add ContextConfigureExtentionMethod

---
+++ Refhub_Ir/Tools/ExtentionMethod/ContextConfigureExtentionMethod.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Refhub_Ir.Data.Context;
+using Refhub_Ir.Tools.Static;
+
+namespace Refhub_Ir.Tools.ExtentionMethod
+{
+    public static class ContextConfigureExtentionMethod
+    {
+
+        public static IServiceCollection ConfigureContext(this IServiceCollection service,ConfigurationManager configuration)
+        {
+
+
+            service.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            return service;
+        }
+    }
+}
