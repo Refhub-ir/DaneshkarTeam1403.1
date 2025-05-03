b83ffd35b4b249d804553ceb536af69ebcffa061
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-03 15:53:33 +0330

    add IdentityConfigureExtentionMethod

---
+++ Refhub_Ir/Tools/ExtentionMethod/IdentityConfigureExtentionMethod.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Refhub_Ir.Data.Context;
+using Refhub_Ir.Tools.Static;
+
+namespace Refhub_Ir.Tools.ExtentionMethod
+{
+    public static class IdentityConfigureExtentionMethod
+    {
+        public static IServiceCollection ConfigureIdentity(this IServiceCollection service)
+        {
+            service.AddIdentity<ApplicationUser, IdentityRole>(options =>
+                {
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireDigit = true;
+                    options.Password.RequiredLength = 6;
+                })
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            return service;
+        }
+
+    }
+}
