c6342da3b20e61d0e6f511cde6c6cc7382ec1f61
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-03 15:53:23 +0330

    add CookieConfigureExtentionMethod

---
+++ Refhub_Ir/Tools/ExtentionMethod/CookieConfigureExtentionMethod.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Refhub_Ir.Data.Context;
+using Refhub_Ir.Tools.Static;
+
+namespace Refhub_Ir.Tools.ExtentionMethod
+{
+    public static class CookieConfigureExtentionMethod
+    {
+
+        public static IServiceCollection ConfigureCookie(this IServiceCollection service)
+        {
+       
+
+            service.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Account/Login";
+                options.AccessDeniedPath = "/Account/AccessDenied";
+                options.Cookie.Name = "RefHub.AuthCookie";
+            });
+            return service;
+        }
+    }
+}
