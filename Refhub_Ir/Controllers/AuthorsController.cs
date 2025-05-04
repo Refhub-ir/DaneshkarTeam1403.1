45d678efff125b78a43aebbc1b0d36a5ce26a592
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-05-04 11:19:15 +0330

    Add AuthorsController

---
+++ Refhub_Ir/Controllers/AuthorsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Refhub_Ir.Service.Interface;
+
+namespace Refhub_Ir.Controllers
+{
+    public class AuthorsController(IAuthorService authorService) : Controller
+    {
+       
+    }
+}
