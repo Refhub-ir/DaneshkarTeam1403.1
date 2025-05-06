e5ea1f82dacd878807355632a36b59b0727a4890
mohammad mehdi khodabande <mmahdikhodabande@gmail.com>
2025-05-06 15:46:23 +0330

    Add controller and use the service for list of books

--- Refhub_Ir/Controllers/BookController.cs
+++ Refhub_Ir/Controllers/BookController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Refhub_Ir.Service.Implement;
 using Refhub_Ir.Service.Interface;
 
 namespace Refhub_Ir.Controllers
 {
     public class BookController(IBookService bookService) : Controller
     {
-      
+        private readonly int _pageSize = 3;
+        public async Task<IActionResult> Index(string searchText, string authorFilter, string categoryFilter, int page = 1, CancellationToken cancellationToken = default)
+        {
+            var viewModel = await bookService.GetListAsync(searchText, authorFilter, categoryFilter,_pageSize, page,cancellationToken);
+
+            return View(viewModel);
+        }
+
     }
 }
