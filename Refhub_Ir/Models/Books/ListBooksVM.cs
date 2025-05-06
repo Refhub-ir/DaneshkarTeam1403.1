8036a2a150fb105a969a89e66787669d35fff49f
mohammad mehdi khodabande <mmahdikhodabande@gmail.com>
2025-05-06 15:45:50 +0330

    Add VM about Book

---
+++ Refhub_Ir/Models/Books/ListBooksVM.cs
@@ -0,0 +1,26 @@
+﻿namespace Refhub_Ir.Models.Books
+{
+    public class ListBooksVM
+    {
+        public List<BookVM> Books { get; set; }
+        public List<Category> Categories { get; set; } 
+        public List<Author> Authors { get; set; } 
+
+
+        public string AuthorFilter { get; set; }
+        public string CategoryFilter { get; set; }
+
+       
+        
+
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public string? SearchText { get; set; }
+
+        public bool HasPreviousPage => CurrentPage > 1;
+        public bool HasNextPage => CurrentPage < TotalPages;
+
+        public int PreviousPage => CurrentPage - 1;
+        public int NextPage => CurrentPage + 1;
+    }
+}
