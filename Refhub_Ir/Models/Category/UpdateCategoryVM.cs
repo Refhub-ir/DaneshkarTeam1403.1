63e9588c16644e6ec2e47bb8647fe3e3637edb98
Mohammad Faraji <m.faraji9841@gmail.com>
2025-05-06 18:15:44 +0330

    Adde Data Anotaion In View Model UpdateCategoryVM

--- Refhub_Ir/Models/Category/UpdateCategoryVM.cs
+++ Refhub_Ir/Models/Category/UpdateCategoryVM.cs
@@ -1,10 +1,15 @@
-﻿namespace Refhub_Ir.Models.Categories
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Refhub_Ir.Models.Categories
 {
     public class UpdateCategoryVM
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "لطفا نام دسته بندی را وارد کنید.")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "لطفا نام اسلاگ را وارد کنید.")]
         public string Slug { get; set; }
+        [Required(ErrorMessage = "لطفا توضیحات دسته بندی را وارد کنید.")]
         public string Description { get; set; }
     }
 }
