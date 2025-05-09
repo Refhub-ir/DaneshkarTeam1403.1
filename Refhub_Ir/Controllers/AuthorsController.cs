bbbfab0f3c87e70bb60977a473c23ee0658827ca
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-08 17:18:28 -0800

    implimented Context

--- Refhub_Ir/Controllers/AuthorsController.cs
+++ Refhub_Ir/Controllers/AuthorsController.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Refhub_Ir.Data.Context;
 using Refhub_Ir.Service.Interface;
 
 namespace Refhub_Ir.Controllers
 {
-    public class AuthorsController(IAuthorService authorService) : Controller
+    public class AuthorsController(IAuthorService authorService, AppDbContext context) : Controller
     {
        
     }
